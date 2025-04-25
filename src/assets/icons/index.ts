7bc1f4f82267b9c3573885dfe58e99f7586596e4
chaeyoung103 <thdcodud103@naver.com>
2025-04-25 17:04:16 +0900

    feat: add images in detail page

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import BackIcon from "./backArrow.svg?react";
+import AlarmIcon from "./alarm.svg?react";
+import TrashIcon from "./trash.svg?react";
+import AciveAlarmIcon from "./alarm_active.svg?react";
+
+export { BackIcon, AlarmIcon, TrashIcon, AciveAlarmIcon };
