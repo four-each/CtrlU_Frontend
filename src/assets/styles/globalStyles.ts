02cebcf111b2fe37edf9c9cbdf6bf158b249b5c5
chaeyoung103 <thdcodud103@naver.com>
2025-04-25 17:01:59 +0900

    feat: apply font

--- src/assets/styles/globalStyles.ts
+++ src/assets/styles/globalStyles.ts
@@ -1,26 +1,21 @@
 import { css } from "@emotion/react";
 
 export const globalStyles = css`
-  /* @font-face {
-    font-family: "JejuGothic";
-    src: url("/fonts/JejuGothicOTF.otf") format("opentype");
-    font-weight: normal;
+  @font-face {
+    font-family: "NotoSansKR";
+    src: url("/fonts/NotoSansKR-VariableFont_wght.ttf") format("ttf");
+    font-weight: 100 900;
+    font-display: swap;
     font-style: normal;
   }
-  @font-face {
-    font-family: "JejuMyeongjo";
-    src: url("fonts/JejuMyeongjoOTF.otf") format("opentype");
-    font-weight: normal;
-    font-style: normal;
-  } */
   * {
     box-sizing: border-box;
-    font-family: "JejuGothic", "JejuMyeongjo", sans-serif;
+    font-family: "NotoSansKR", sans-serif;
   }
 
   html {
     box-sizing: border-box;
-    font-family: "JejuGothic", "JejuMyeongjo", sans-serif;
+    font-family: "NotoSansKR", sans-serif;
     font-size: 62.5%;
   }
 
@@ -31,8 +26,9 @@ export const globalStyles = css`
     min-height: 100vh; /* 기본 브라우저 */
     min-height: -webkit-fill-available; /* iOS Safari 뷰포트 채우기 */
     min-height: 100dvh;
+    max-width: 960px;
     overflow-x: hidden;
-    margin: 0;
+    margin: 0 auto;
     padding: 0;
     background-color: white;
 
