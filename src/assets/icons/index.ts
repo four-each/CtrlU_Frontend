c17784faba07e3fba079007d94873bc1cf04a574
정소민 <yjk70702002@gmail.com>
2025-08-08 21:05:18 +0900

    move: .svg 파일 패키지 분리

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,12 +5,12 @@ import AciveAlarmIcon from "./alarm_active.svg?react";
 import ClockIcon from "./clock.svg?react";
 import CTRULogo from "./ctru_logo.svg?react";
 import Owl from "./owl.svg?react";
-import AddFriendIcon from "./add_friend.svg?react";
-import SearchIcon from "./search.svg?react";
-import AlarmLightIcon from "./alarm_light.svg?react";
 import BackLightIcon from "./backArrow_light.svg?react";
-import AcceptIcon from "./accept.svg?react";
-import RemoveIcon from "./remove.svg?react";
+import AddFriendIcon from "./friendship/add_friend.svg?react";
+import SearchIcon from "./friendship/search.svg?react";
+import AlarmLightIcon from "./friendship/alarm_light.svg?react";
+import AcceptIcon from "./friendship/accept.svg?react";
+import RemoveIcon from "./friendship/remove.svg?react";
 
 export {
   BackIcon,
