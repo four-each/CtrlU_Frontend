20ac8aa807f9eaa84797089d715877cecac24035
정소민 <yjk70702002@gmail.com>
2025-08-08 21:53:22 +0900

    remove:  사용 안하는 svg 파일 제거

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,9 @@
 import BackIcon from "./backArrow.svg?react";
-import AlarmIcon from "./alarm.svg?react";
+import BackLightIcon from "./backArrow_light.svg?react";
 import TrashIcon from "./trash.svg?react";
-import AciveAlarmIcon from "./alarm_active.svg?react";
 import ClockIcon from "./clock.svg?react";
 import CTRULogo from "./ctru_logo.svg?react";
 import Owl from "./owl.svg?react";
-import BackLightIcon from "./backArrow_light.svg?react";
 import ImageUploadIcon from "./image_upload.svg?react";
 import AddFriendIcon from "./friendship/add_friend.svg?react";
 import SearchIcon from "./friendship/search.svg?react";
@@ -19,9 +17,7 @@ import SettingIcon from "./mypage/setting.svg?react";
 
 export {
   BackIcon,
-  AlarmIcon,
   TrashIcon,
-  AciveAlarmIcon,
   ClockIcon,
   CTRULogo,
   Owl,
