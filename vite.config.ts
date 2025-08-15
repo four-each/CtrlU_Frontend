fb4236a5b07c9ad427850324047279b753783af4
정소민 <yjk70702002@gmail.com>
2025-08-15 16:39:55 +0900

    feat: cors 설정

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,13 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     host: true,
+    port: 3000,
+    proxy: {
+      // '/api'로 시작하는 모든 요청을 백엔드 서버로 전달
+      '/': {
+        target: 'https://api.ctrlu.site', // 백엔드 서버의 주소
+        changeOrigin: true, // 대상 서버의 출처를 변경
+      },
+    },
   }
 });
