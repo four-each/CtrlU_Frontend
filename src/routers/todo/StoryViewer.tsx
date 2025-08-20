ad10e66989ce32e3a2235da74d4244882e57ff13
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-08-20 23:18:12 +0900

    fix: StoryViewer 헤더 잘림 문제 수정
    - AnimatedCol에서 overflow-y: auto와 padding-bottom 제거
    - Detail.tsx와 동일한 구조로 변경하여 헤더가 정상 표시되도록 수정

--- src/routers/todo/StoryViewer.tsx
+++ src/routers/todo/StoryViewer.tsx
@@ -208,8 +208,6 @@ const Container = styled.div`
 const AnimatedCol = styled(Col)<{ animation: 'left' | 'right' | 'none' }>`
   height: 100%;
   animation: ${({ animation }) => animation === 'left' ? slideInLeft : animation === 'right' ? slideInRight : 'none'} 0.5s forwards;
-  overflow-y: auto;
-  padding-bottom: 40px;
 `;
 
 const HeaderContainer = styled.div`
